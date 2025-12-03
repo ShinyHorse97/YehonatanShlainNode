c6e3f136749073a626d3b933663628990c8af1a9
tihonist <tihonist@DESKTOP-BVCGH54>
2025-12-03 10:34:52 +0200

    p

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AllClassesNodes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,15 @@ namespace YehonatanShlainNode
 {
     internal class Program
     {
+        public static Node<int> CreateList(int count)
+        {
+            Node<int> n = null, pos = null;
+            in
+            n = new Node<int>(int.Parse(Console.ReadLine()));
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }
