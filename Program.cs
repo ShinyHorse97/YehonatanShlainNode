3878e4362c2f6ffe163164775e29aa812cb0a5c2
tihonist <tihonist@DESKTOP-BVCGH54>
2025-12-04 15:33:17 +0200

    push 2 +3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using AllClassesNodes;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,12 +49,44 @@ namespace YehonatanShlainNode
             }
             return n;
         }
+        public static Node<int> CreateNodeFromArray(int[] arr)
+        {
+            Node<int> head = null;
+            Node<int> tail = null;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Node<int> newNode = new Node<int>(arr[i]);
+                if (head == null)
+                {
+                    head = newNode;
+                    tail = newNode;
+                }
+                else
+                {
+                    tail.SetNext(newNode);
+                    tail = newNode;
+                }
+            }
+            return head;
+        }
 
+        public static Node<int> ReverseNodeFromArray(int[] arr)
+        {
+            Node<int> n = null;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                n = new Node<int>(arr[i], n);
+            }
+            return n;
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine(CreateList());
             Console.WriteLine(ReverseList());
+            int[] sampleArray = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(CreateNodeFromArray(sampleArray));
+            Console.WriteLine(ReverseNodeFromArray(sampleArray));
         }
     }
 }
