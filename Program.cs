1406829f4ed2cb1788eaf49c576ffb7a656dca9b
tihonist <tihonist@DESKTOP-BVCGH54>
2025-12-04 14:34:48 +0200

    targil 1 + 2

--- Program.cs
+++ Program.cs
@@ -9,15 +9,51 @@ namespace YehonatanShlainNode
 {
     internal class Program
     {
-        public static Node<int> CreateList(int count)
+        public static Node<int> CreateList()
         {
-            Node<int> n = null, pos = null;
-            in
-            n = new Node<int>(int.Parse(Console.ReadLine()));
+            Console.WriteLine("insert a number (-999) to stop");
+            int num = int.Parse(Console.ReadLine());
+            Node<int> head = null;
+            Node<int> tail = null;
+
+            while (num != -999)
+            {
+                Node<int> newNode = new Node<int>(num);
+                if (head == null)
+                {
+                    head = newNode;
+                    tail = newNode;
+                }
+                else
+                {
+                    tail.SetNext(newNode);
+                    tail = newNode;
+                }
+                Console.WriteLine("insert a number (-999) to stop");
+                num = int.Parse(Console.ReadLine());
+            }
+            return head;
         }
+
+        public static Node<int> ReverseList()
+        {
+            Console.WriteLine("insert a number (-999) to stop");
+            int num = int.Parse(Console.ReadLine());
+            Node<int> n = null;
+            while (num != -999)
+            {
+                n = new Node<int>(num, n);
+                Console.WriteLine("insert a number (-999) to stop");
+                num = int.Parse(Console.ReadLine());
+            }
+            return n;
+        }
+
+
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(CreateList());
+            Console.WriteLine(ReverseList());
         }
     }
 }
